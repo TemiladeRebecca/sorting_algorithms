3a5ccdd1b0aa05fc9d8a94df4cac84e518e28e68
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-07-24 11:35:01 -0700

    A function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,41 @@
 #include "sort.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Return: void
+ * bubble_sort - sorts an arr in ascending order using bubble sort
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n, new_n;
-	int swap;
+	size_t w, y;
+	int temp;
 
-	if (array == NULL || size < 2)
+	/**
+	 * The outer for loop iterates over the array/ its the passes
+	 * w < size - 1, ensures the loop will iterate over the array
+	 * until w reaches the 2nd last index. It ensures that during
+	 * each iteration for outer loop, the algorithm can compare
+	 * each element with its adjacent element and perform the
+	 * neccessary swap if the order is incorrect
+	 * The inner loop iterates only over unsorted array hence the
+	 * y < size - 1 - w
+	 */
+	if (size < 2)
 		return;
 
-	n = size;
-	while (n > 0)
+	for (w = 0; w < size - 1; w++)
 	{
-		new_n = 0;
-		for (i = 0; i < n - 1; i++)
+		for (y = 0; y < size - 1 - w; y++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[y] > array[y + 1])
 			{
-				swap = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = swap;
-				new_n = i + 1;
+				temp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = temp;
 				print_array(array, size);
 			}
 		}
-		n = new_n;
 	}
 }
